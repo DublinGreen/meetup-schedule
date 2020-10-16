c6b735c7f1e7f960c5fcb9927def1ef3529fc252
Idisimagha Dublin-Green <greendublin007@gmail.com>
2020-10-17 00:40:16 +0100

    Create solution.php
    update

---
+++ solution.php
@@ -0,0 +1,28 @@
+<?php
+
+
+/*
+ * Complete the 'countMeetings' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY firstDay
+ *  2. INTEGER_ARRAY lastDay
+ */
+
+function countMeetings($firstDay, $lastDay) {
+    // Write your code here    
+    if(!empty($firstDay) && !empty($lastDay)){
+        //print_r($firstDay);
+        //print_r($lastDay);
+        //echo("firstDay Count : " . count($firstDay));
+        //echo("lastDay Count : " . count($lastDay));
+
+        $result = array_unique(array_merge($firstDay, $lastDay));
+        return count($result);        
+    }else{
+        return 0;
+    }
+
+}
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
